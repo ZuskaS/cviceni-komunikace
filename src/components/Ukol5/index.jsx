5c4b7631471f92169c7f924cd4a606691dfb2537
Zuzana Ilečková <yes>
2025-05-10 16:01:28 +0200

    dokončení 5.úkolu

--- src/components/Ukol5/index.jsx
+++ src/components/Ukol5/index.jsx
@@ -1,4 +1,5 @@
-import { InputField } from "./InputField";
+import { InputField } from './InputField';
+import { useState } from 'react';
 
 /*
 Zadání: Vyřešte tento úkol podobně jako předchozí, pouze s tím rozdílem, že tentokrát budete mít
@@ -7,11 +8,25 @@ Zadání: Vyřešte tento úkol podobně jako předchozí, pouze s tím rozdíle
 */
 
 export const Ukol5 = () => {
+  const [jmeno, setJmeno] = useState('');
+  const [prijmeni, setPrijmeni] = useState('');
   return (
     <>
-      <p>Jméno: Patrik Pokoutník</p>
-      <InputField label="Jméno" type="text" />
-      <InputField label="Příjmení" type="text" />
+      <p>
+        Jméno: {jmeno} {prijmeni}
+      </p>
+      <InputField
+        label="Jméno"
+        type="text"
+        value={jmeno}
+        onValueChange={setJmeno}
+      />
+      <InputField
+        label="Příjmení"
+        type="text"
+        value={prijmeni}
+        onValueChange={setPrijmeni}
+      />
     </>
   );
 };
